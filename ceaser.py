da4d5a261f751bff1e43b956b3a55fe330a74fe4
Потапов Даниил <113824271+potopik@users.noreply.github.com>
2022-10-09 23:53:37 +0500

    Интерфейс + Комментарии

--- ceaser.py
+++ ceaser.py
@@ -1,33 +1,41 @@
-alfabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-vibor = str(input('Если вы хотите провести шифровку введите y, если дешифровку n '))
+def is_digit(num_str): # функция на проверку корректности шага(введения только цифр)
+    return num_str.replace('.', '', 1).lstrip('-').isdigit()
+
+
+alfabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя' # Задаем переменную с двойным алфавитом для шифрования последних букв
+vibor = str(input('Если вы хотите провести шифровку введите y, если дешифровку n ')) # Задаем переменную для выбора расшифровки или шифровки
 itog = ''
-if vibor == 'Y' or vibor == 'y':
-    shag = int(input('Введите шаг сдвига от 1 до 33'))
-    if shag <= 0 or shag > 33:
-        print('Некорректный шаг')
-    else:
+if vibor == 'Y' or vibor == 'y': #Блок предназначенный для шифровки
+    shag = input('Введите шаг сдвига от 1 до 33')
+    shag = is_digit(shag)
+    if (shag > 0) and (shag <= 33):
         phrase = str(input('Введите высказывание  для шифрования'))
-        for i in phrase:
-            position = alfabet.find(i)
-            position2 = position + shag
+        phrase = phrase.lower() # Превращение верхнего регистра в нижний
+        for i in phrase: # Цикл for, чтобы обратится к каждой букве по её номеру
+            position = alfabet.find(i) # Обращается к элементам alfabet, для поиска совпадений
+            position2 = position + shag # К индексу буквы добавляем шаг
             if i in alfabet:
-                itog = itog + alfabet[position2]
+                itog = itog + alfabet[position2]  # Внесение буквы в новую строку
             else:
-                itog = itog + i
+                itog = itog + i # Внесение символа из не русского алфавита
         print('Зашифрованное сообщение', itog)
-elif vibor == 'N' or vibor == 'n':
-    shag = int(input('Введите шаг сдвига от 1 до 33'))
-    if shag <= 0 or shag > 33:
-        print('Некорректный шаг')
     else:
+        print('Некорректный шаг')
+elif vibor == 'N' or vibor == 'n':# Блок предназначенный для расшифровки
+    shag = input('Введите шаг сдвига от 1 до 33')
+    shag = is_digit(shag)
+    if (shag > 0) and (shag <= 33):
         phrase = str(input('Введите высказывание  для дешифрования'))
+        phrase = phrase.lower()
         for i in phrase:
             position = alfabet.find(i)
-            position2 = position - shag
+            position2 = position - shag # Еинственное отличие от предыдущего блока: от индекса буквы отнимаем шаг
             if i in alfabet:
                 itog = itog + alfabet[position2]
             else:
                 itog = itog + i
         print('Расшифрованное сообщение', itog)
+    else:
+        print('Некорректный шаг')
 else:
     print('Некорректная команда')
